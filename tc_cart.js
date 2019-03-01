fbd8208b8dcde239d8686bbe2fb43a62188417da
Unknown <TAdcox009@west-mec.org>
2019-03-01 16:10:34 -0700

    final commit
    Debugged code, final commit, ready for review.

--- tc_cart.js
+++ tc_cart.js
@@ -25,13 +25,13 @@ for (var i = 0; i <= 3; i++) {
 
     // Adds the following HTML code to the value of the cartHTML variable
     cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "'/> </td>";
-    cartHTML += "<td>" + itemDescription[i] + "</td> <td$>" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
     // A variable named itemCost is being set equal to the itemPrice multiplied by the itemQty variables.
     var itemCost = itemPrice[i] * itemQty[i];
-    cartHTML += "<td$>" + itemCost + "</td></tr>";
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
     orderTotal = orderTotal + itemCost;
 }
 
-// 14. Apply the cartHTML value to the inner HTML of the div element with the ID cart.
-cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td$>" + orderTotal + "</td> </tr> </table>";
+// Apply the cartHTML value to the inner HTML of the div element with the ID cart.
+cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
 document.getElementById("cart").innerHTML = cartHTML;
