370b589a300fec82607c42f2a6cbd72d05ede8a8
Unknown <TAdcox009@west-mec.org>
2019-02-28 23:40:50 -0700

    commit 4
    Tried to debug code.

--- tc_cart.js
+++ tc_cart.js
@@ -15,7 +15,7 @@
 
 
 //A variable named orderTotal, its initial value is set equal to 0.
-var oderTotal = 0;
+var orderTotal = 0;
 
 // A variable named cartHTML that contains a string of some HTML code that will create a table
 var cartHTML = "<table> <tr> <th>Item</th> <th>Description</th> <th>Price</th> <th>Qty</th> <th>Total</th> </tr>";
@@ -24,7 +24,7 @@ var cartHTML = "<table> <tr> <th>Item</th> <th>Description</th> <th>Price</th> <
 for (var i = 0; i <= 3; i++) {
 
     // Adds the following HTML code to the value of the cartHTML variable
-    cartHTML += "<tr> <td> <img src = 'tc_" + item[i] +  ".png' alt = '" + item[i] + "'/>" + "</td>";
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "'/> </td>";
     cartHTML += "<td>" + itemDescription[i] + "</td> <td$>" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
     // A variable named itemCost is being set equal to the itemPrice multiplied by the itemQty variables.
     var itemCost = itemPrice[i] * itemQty[i];
@@ -34,4 +34,4 @@ for (var i = 0; i <= 3; i++) {
 
 // 14. Apply the cartHTML value to the inner HTML of the div element with the ID cart.
 cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td$>" + orderTotal + "</td> </tr> </table>";
-document.getElementById1("cart").innerHTML = cartHTML;
+document.getElementById("cart").innerHTML = cartHTML;
